ea7e5bb1918ee211dba740412a9e3b0deab17538
JH <59975543+JH1814@users.noreply.github.com>
2021-09-01 15:09:01 +0000

    Tried to code

---
+++ Multiplication.java
@@ -0,0 +1,14 @@
+class main{
+  public static void main (String[] args){
+    
+    int num1 = 3;
+    int num2 = 6;
+    int sum  = 0;
+    
+    if (sum <= num2){
+      sum = sum + num1;
+    } else {
+      System.out.println(sum);
+    }
+  }
+}
