de17d10adba2ee617cb2936ed603e3340d830a0d
JH <59975543+JH1814@users.noreply.github.com>
2021-09-02 10:59:29 +0200

    Update

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+// Aufruf der Funktion Ausgeben
+    Ausgeben("Hello World");
+  }
+  public static void Ausgeben(String AusgabeText){
+    System.out.println(AusgabeText);
   }
 }
 
