c5df256d7115c39c6df0f211e511eeda89609e90
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-10-19 10:52:33 +0200

    delete files

--- Primzahl.java
+++
@@ -1,7 +0,0 @@
-//2, 3, 7, 13, 17, 23, 31, 37, 43, 53, 59, 67, 73, 79, 89
-
-public class Primzahl {
-    public static void Primzahl(){
-
-    }
-}
--- Variables.java
+++
@@ -1,5 +0,0 @@
-public class Variables {
-    public static void Name(String Name){
-        System.out.println("Hello " + Name);
-    }
-}
