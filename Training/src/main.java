f946e34e2a0aacda964172b78bc8d893c2396fb6
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-10-24 22:29:39 +0200

    prepared array

---
+++ Training/src/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        int[] asdf = new int[10];
+        int[] wasd = new int[5];
+
+        for (int zaehler = 0; zaehler < 5; zaehler++)
+        {
+            asdf [zaehler] = zaehler + 5;
+            System.out.println(asdf[zaehler]);
+
+        }
+    }
+}
