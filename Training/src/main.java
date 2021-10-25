89f0db15383d2ca8f1ffdbce38ea02c8756da5c3
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-10-25 15:52:57 +0200

    Update main.java

--- Training/src/main.java
+++ Training/src/main.java
@@ -1,13 +1,14 @@
 public class main {
+    int betrag = 
     public static void main(String[] args) {
-        int i = plus(5, 8);
+
         System.out.println(i);
 
 
     }
     private static int plus(int a, int b)
     {
-        return a+b;
+
     }
 
 }
