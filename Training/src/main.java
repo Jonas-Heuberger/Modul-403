21388ba8f665cb5c4b991ab7a8292dfd4ef5415a
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-10-25 17:00:30 +0200

    one way to solve it

--- Training/src/main.java
+++ Training/src/main.java
@@ -1,14 +1,40 @@
-public class main {
-    int betrag = 
+public class main<ATMhunderter> {
     public static void main(String[] args) {
-
-        System.out.println(i);
+        int ATMt = berechneTausender(4321);
+        int ATM2h = berechneZweiHunderter(ATMt);
+        int ATMh = berechneHunderter(ATM2h);
+        System.out.println(ATMt);
+        System.out.println(ATM2h);
+        System.out.println(ATMh);
 
 
     }
-    private static int plus(int a, int b)
-    {
+
+    private static int berechneTausender(int betrag) {
+        int ATMtausender = 0;
+        while (betrag > 1000) {
+            betrag = betrag - 1000;
+            ATMtausender++;
+        }
+        return ATMtausender;
 
     }
 
+    private static int berechneZweiHunderter(int ATMt) {
+        int AtmZweiunderter = 0;
+        while (ATMt > 200) {
+            ATMt = ATMt - 200;
+            AtmZweiunderter++;
+        }
+        return AtmZweiunderter;
+    }
+
+    private static int berechneHunderter(int ATM2h) {
+        int AtmHunderter = 0;
+        while (ATM2h > 100) {
+            ATM2h = ATM2h - 100;
+            AtmHunderter++;
+        }
+        return AtmHunderter;
+    }
 }
