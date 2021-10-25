c71941bfc99a91d461c1d03a8c9b10985c933afb
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-10-25 11:30:03 +0200

    simple math

--- Training/src/main.java
+++ Training/src/main.java
@@ -1,7 +1,12 @@
 public class main {
     public static void main(String[] args) {
-        
+        System.out.println(plus(5, 8));
 
 
     }
+    private static int plus(int a, int b)
+    {
+        return a+b;
+    }
+
 }
