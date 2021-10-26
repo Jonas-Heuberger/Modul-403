6a9a79619dc8ee8ffdf239017c23c34075922daa
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-10-26 14:13:25 +0200

    Created method Abfrage

--- Abschreiben/src/main.java
+++ Abschreiben/src/main.java
@@ -1,5 +1,12 @@
 public class main {
     public static void main(String[] args)  {
-        
+
     }
-}
+    private static double Abfrage(){ 
+        Scanner scan = new Scanner(System.in);
+        double Zahl = scan.nextDouble();
+        return Zahl; 
+    }
+
+    }
+
