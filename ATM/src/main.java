9bc9bd1b7e769c7e038b610b6f36e11785dde4da
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-10-27 22:58:25 +0200

    changed while loops to for loops

--- ATM/src/main.java
+++ ATM/src/main.java
@@ -16,37 +16,46 @@ public class main {
     public static int[] berechnen(int betrag){
         int[]Noten = new int[9];
 
-        while (betrag >= 1000){
+        for (Noten[0] = 0; betrag >= 1000; Noten[0]++){
             betrag = betrag - 1000;
-            Noten[0]++;
-        }while (betrag >= 200){
+        }
+        
+        for (Noten[1] = 0; betrag >= 200; Noten[1]++){
             betrag = betrag - 200;
-            Noten[1]++;
-        }while (betrag >= 100){
+        }
+        
+        for (Noten[2] = 0; betrag >= 100; Noten[2]++){
             betrag = betrag - 100;
-            Noten[2]++;
-        }while (betrag >= 50){
+        }
+        
+        for (Noten[3] = 0; betrag >= 50; Noten[3]++){
             betrag = betrag - 50;
-           Noten[3]++;
-        }while (betrag >= 20) {
+        }
+        
+        for (Noten[4] = 0; betrag >= 20; Noten[4]++){
             betrag = betrag - 20;
-            Noten[4]++;
-        }while (betrag >= 10){
+        }
+        
+        for (Noten[5] = 0; betrag >= 10; Noten[5]++){
             betrag = betrag - 10;
-            Noten[5]++;
-        }while (betrag >= 5){
+        }
+        
+        for (Noten[6] = 0; betrag >= 5; Noten[6]++){
             betrag = betrag - 5;
-            Noten[6]++;
-        }while (betrag >= 2){
+        }
+        
+        for (Noten[7] = 0; betrag >= 2; Noten[7]++){
             betrag = betrag - 2;
-            Noten[7]++;
-        }while (betrag >= 1){
+        }
+        
+        for (Noten[8] = 0; betrag >= 1; Noten[8]++){
             betrag = betrag - 1;
-            Noten[8]++;
         }
 
        return Noten;
     }
+
+    
     private static void Ausgabe(int[] Noten){
         System.out.println("Tausender: " + Noten[0]);
         System.out.println("Zweihunderter: " + Noten[1]);
