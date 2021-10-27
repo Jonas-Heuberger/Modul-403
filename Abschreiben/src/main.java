b82d473288808c67ceea2fae2c94901013b62244
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-10-27 09:26:20 +0200

    insert a function call

--- Abschreiben/src/main.java
+++ Abschreiben/src/main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args)  {
 
+        double betrag = Abfrage();
+
     }
     private static double Abfrage(){ 
         Scanner scan = new Scanner(System.in);
