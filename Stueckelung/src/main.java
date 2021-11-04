099564e6001600b5aa2affd3ed4e9ca7eeb7283e
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-11-04 11:09:03 +0100

    update

--- Stueckelung/src/main.java
+++ Stueckelung/src/main.java
@@ -1,34 +1,111 @@
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class main {
+    public static final int  ANZAHL_EINHEITEN = 5;
+
+    public static String Einheiten[];
+    public static int Faktoren[];
+    public static int Loesung[] = new int[ANZAHL_EINHEITEN];
+
     public static void main(String[] args) {
-        String unit = unit();
-        int value = value();
-        int result = calculate(value, unit);
+        System.out.println("Umrechner gestartet");
 
+        //Eingaben
+        int anzahl = LesenAnzahl();
+        String einheit = LesenEinheit();
 
+        //Ermitteln der Position im korrekten Array
+        int position = CalcArrays(einheit);
 
+        //Ermitteln des Basiswertes
+        int basis = CalcBasis(anzahl, position);
+
+        //Aufteilen
+        CalcSplitting(basis);
+
+        //Ausgeben
+        OutputSplitting();
     }
 
-    private static String unit(){
-        Scanner scan = new Scanner(System.in);
-        String unit = scan.nextLine();
-        return unit;
+    public static int LesenAnzahl()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welche Zahl soll umgerechnet werden?");
+        int zahl = scanner.nextInt();
+        return zahl;
     }
 
-    
-private static int value(){
-    Scanner scan = new Scanner(System.in);
-    int value = scan.nextInt();
-    return value;
-}
-public static int[] calculate(int value,String unit){
-    String[] Längenmasse = new String[]{"mm", "cm", "dm", "m", "km"};
-    int[] Längenmasse = new int[]{0, 10, 100, 1000, 1000000};
-    String[] Hohlmasse = new String[] {"ml", "cl", "dl", "l", "hl"};
-    int[] Hohlmasse = new int[]{0,10,100, 1000, 100000};
-    
+    public static String LesenEinheit()
+    {
+        Scanner scanner = new Scanner(System.in);
 
-}
+        System.out.println("Welche Masseinheit soll umgerechnet werden?");
+        String einheit = scanner.nextLine();
+        return einheit;
+    }
+
+    public static int CalcArrays(String einheit)
+    {
+        String[] einheitenLaengen = new String[]{"mm", "cm", "dm", "m", "km"};
+        int[] faktorenLaengen = new int[]{ 1, 10, 100, 1000, 1000000 };
+        String[] einheitenHohl = new String[]{"ml", "cl", "dl", "l", "hl"};
+        int[] faktorenHohl = new int[]{ 1, 10, 100, 1000, 100000 };
+        String[] einheitenGewicht = new String[]{"mg", "g", "kg", "c", "t"};
+        int[] faktorenGewicht = new int[]{ 1, 1000, 1000000, 1000000000, 1000000000 };
+        for (int i = 0; i < ANZAHL_EINHEITEN; i++)
+        {
+            if (einheitenLaengen[i].equals(einheit))
+            {
+                Faktoren = faktorenLaengen;
+                Einheiten = einheitenLaengen;
+                return i;
+            }
+
+            if (einheitenHohl[i].equals(einheit))
+            {
+                Faktoren = faktorenHohl;
+                Einheiten = einheitenHohl;
+                return i;
+            }
+            if (einheitenGewicht[i].equals(einheit))
+            {
+                Faktoren = faktorenGewicht;
+                Einheiten = einheitenGewicht;
+                return i;
+            }
+
+        }
+
+        System.out.println("Masseinheit unbekannt");
+        System.exit(0);
+        return -1;
+    }
+
+    public static int CalcBasis(int anzahl, int position)
+    {
+        int faktor = Faktoren[position];
+        return anzahl * faktor;
+    }
+
+    public static void CalcSplitting(int basis)
+    {
+        int rest = basis;
+        for (int i = ANZAHL_EINHEITEN-1; i>=0; i--)
+        {
+            Loesung[i] = rest / Faktoren[i];
+            rest = rest % Faktoren[i];
+
+            if (rest == 0)
+                break;
+        }
+    }
+
+    public static void OutputSplitting()
+    {
+        for (int i = ANZAHL_EINHEITEN-1; i>=0; i--)
+        {
+            System.out.println(Einheiten[i] + ": " + Loesung[i]);
+        }
+    }
 }
