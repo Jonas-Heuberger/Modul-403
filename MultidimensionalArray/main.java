8afa7ebd21ed5755e2b1de99c566fab3b7e5adf5
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-11-10 20:54:47 +0100

    Update main.java

--- MultidimensionalArray/main.java
+++ MultidimensionalArray/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        int[ ][ ] sample = { {1, 2, 3}, {4, 5, 6} }; 
-        int x = sample[1][0];
+        int[ ][ ] sample = { {1, 2, 3}, {4, 5, 6} };  // Array 0 und 1
+        int x = sample[1][0]; // greift auf Array 1 auf position 0 zu
         System.out.println(x);
     }   
 }
