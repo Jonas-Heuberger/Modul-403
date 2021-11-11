67567ecaa015858e2ea619651298484916b74ad7
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-11-12 00:04:00 +0100

    update

--- MultidimensionalArray/main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        int[ ][ ] sample = { {1, 2, 3}, {4, 5, 6} };  // Array 0 und 1
-        int x = sample[1][0]; // greift auf Array 1 auf position 0 zu
-        System.out.println(x);
-    }   
-}
