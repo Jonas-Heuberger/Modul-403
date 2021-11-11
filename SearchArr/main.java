860edc6e22e669b234571f20402597b0b4891ef7
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-11-11 10:56:15 +0100

    update

--- SearchArr/main.java
+++ SearchArr/main.java
@@ -1,21 +1,22 @@
 public class main {
     public static void main(String[] args) {
-       int num = 8;
+       int num = 50;
        int solArr[] = solveArr(num);
-       System.out.println("The number " + solArr[0] + " and the number " + solArr[1] + " make up " + num);
     }
 
     public static int[] solveArr(int num){
-      int arr[] = {2, 5, 7, 15, 3, 4};
+      int arr[] = {2, 5, 7, 15, 3, 4, 6};
       int solArr[] = new int[2];
 
       /*Your Code*/
       for(int i = 0; i < arr.length; i++){
         
-        for(int j = 1; j < arr.length; j++ ){
-          if (arr[i] + arr[j] == num) {
+        for(int j = i + 1; j < arr.length; j++ ){
+          if (arr[i] + arr[j] == num) { // *, /, -
             solArr[0] = arr[i];
             solArr[1] = arr[j];
+            System.out.println("The number " + solArr[0] + " and the number " + solArr[1] + " add up " + num);
+
           }
 
         }
